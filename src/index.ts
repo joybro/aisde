68b21c9d0459d9fcd3ea2dbf335e302f0d07bcde
Young Han Lee <joybro201@gmail.com>
2023-04-09 18:25:19 +0100

    Include package.json and all ts file contents in query

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import readline from 'readline';
 import fs from 'fs';
+import path from 'path';
 import { ChatOpenAI } from 'langchain/chat_models';
 import {
     HumanChatMessage,
@@ -21,7 +22,7 @@ function readConfig(): { [key: string]: string } {
 async function main() {
     const config = readConfig();
 
-    // Update API_KEY and API_ENDPOINT from config if they exist
+    // Read API_KEY from config file
     if (!config.api_key) {
         console.log('Please provide an API key in aisde.json');
         return;
@@ -31,8 +32,32 @@ async function main() {
 
     console.log('Welcome to the AISDE!');
 
+    // Read index.ts and package.json contents
+    const packageJsonContent = fs.readFileSync('package.json', 'utf-8');
+
+    const srcDir = 'src';
+
+    // Find all ts files in the src directory and read their contents
+    const tsFiles = fs
+        .readdirSync(srcDir)
+        .filter(file => path.extname(file) === '.ts')
+        .map(file => {
+            const path = `${srcDir}/${file}`;
+            return [path, fs.readFileSync(path, 'utf-8')];
+        })
+        .map(
+            ([path, content]) =>
+                new SystemChatMessage(
+                    `Here is the content of ${path}:\n${content}`,
+                ),
+        );
+
     let messages = [
         new SystemChatMessage('You are an AI developer assistant.'),
+        new SystemChatMessage(
+            `Here is the content of package.json:\n${packageJsonContent}`,
+        ),
+        ...tsFiles,
     ];
 
     while (true) {
